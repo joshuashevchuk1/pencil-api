29930b1d688a97e6ec99a92b4570aececbe39854
joshuashevchuk <joshua.shevchuk@ookla.com>
2024-12-03 22:23:40 -0800

    updated test and made it simpler

--- src/app/resolvers/test.py
+++ src/app/resolvers/test.py
@@ -2,9 +2,8 @@ import pencil_init as pi
 
 def test():
     pinit = pi.PencilInitResolver()
-    configurable_file = pinit.build_file()
-    with open("../../../start.in", "w") as file:
-        file.write(configurable_file)
+    pinit.set_file_name("start.in")
+    pinit.write_file()
 
 if __name__ == '__main__':
     test()
