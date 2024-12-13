53d94550d0912f2074269a70560e7170dc8e8f88
joshuashevchuk <joshua.shevchuk@ookla.com>
2024-12-12 22:44:57 -0800

    added commander api file

---
+++ src/api/commander.py
@@ -0,0 +1,15 @@
+from flask import request, jsonify
+
+class PencilCommander:
+    def __init__(self, app):
+        self.app = app
+        return
+
+    def build_sim(self):
+        return
+
+    def start_sim(self):
+        return
+
+    def run_sim(self):
+        return
