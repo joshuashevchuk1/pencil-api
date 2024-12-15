c6e9cb63728cc671af7c79d0b0bef66d610dc0e2
joshuashevchuk <joshua.shevchuk@ookla.com>
2024-12-15 00:37:58 -0800

    add simulation.py commands for resolver

--- src/app/resolvers/simulation.py
+++ src/app/resolvers/simulation.py
@@ -1,6 +1,9 @@
 
 import os
-import config as cfg
+
+from numpy.distutils.command.config import config
+
+import config.config as cfg
 
 
 class SimulationResolver:
@@ -10,18 +13,20 @@ class SimulationResolver:
     def __init__(self):
         self.file_name = None
         self.config = cfg.Config()
+        self.simulation_dir = self.config.get("simulation_dir")
         return
 
     def set_file_name(self,file_name):
         self.file_name = file_name
 
     def build(self):
-        os.system("pc_build")
-
+        os.system("pc_build " + "./" + str(self.simulation_dir))
         return
 
     def start(self):
+        os.system("pc_start " + "./" + str(self.simulation_dir))
         return
 
     def run(self):
+        os.system("pc_run " + "./" + str(self.simulation_dir))
         return
